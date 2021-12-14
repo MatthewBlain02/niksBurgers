cba8575a724bf1a31c7dac4a3ce9e576ea28214e
MatthewBlain02 <53818917+MatthewBlain02@users.noreply.github.com>
2021-12-14 15:48:49 +0000

    New Push

--- js/script.js
+++ js/script.js
@@ -1,47 +1,75 @@
 function addToCart(item){
-let cart = document.getElementById('cart');
-let itemForm = document.getElementById(item+'-form');
-let br = document.createElement('br');
-itemForm.itemName = document.getElementById(item+'-itemName');
-itemForm.price = document.getElementById(item+'-price');
-itemForm.orderDetails = document.getElementsByName(item+'-item');
-itemForm.milkshake = document.getElementsByName(item+'-milkshake');
-let newOrder = document.createElement('div');
-newOrder.classList.add('m-3');
-newOrder.innerHTML = itemForm.itemName.innerHTML;
-newOrder.appendChild(br);
-newOrder.innerHTML += "Order Details";
-newOrder.appendChild(br);
-for(let i = 0; i < itemForm.orderDetails.length;i++){
-    if (itemForm.orderDetails[i].checked === true){
-        newOrder.innerHTML += itemForm.orderDetails[i].value;
+    let itemCounter = document.getElementById('totalItemsInCart');
+    let itemCounterInt = parseInt(itemCounter.innerHTML);
+    if(itemCounterInt == 5){
+        alert('Sorry Due To High Popularity We Can Only Allow You To Order A Max Of 5 Items Per Order');
+    }
+    else{
+        let cart = document.getElementById('cart');
+        let itemForm = document.getElementById(item+'-form');
+        let br = document.createElement('br');
+        itemForm.itemName = document.getElementById(item+'-itemName');
+        itemForm.price = document.getElementById(item+'-price');
+        itemForm.orderDetails = document.getElementsByName(item+'-item');
+        itemForm.milkshake = document.getElementsByName(item+'-milkshake');
+        let newOrder = document.createElement('div');
+        newOrder.classList.add('m-3');
+        newOrder.innerHTML = itemForm.itemName.innerHTML;
+        newOrder.appendChild(br);
+        newOrder.innerHTML += "Order Details";
+        newOrder.appendChild(br);
+        for(let i = 0; i < itemForm.orderDetails.length;i++){
+            if (itemForm.orderDetails[i].checked === true){
+                newOrder.innerHTML += itemForm.orderDetails[i].value;
+                newOrder.appendChild(br);
+
+            }
+        }
+        newOrder.appendChild(br);
         newOrder.appendChild(br);
 
+        for (let i = 0; i < itemForm.milkshake.length;i++){
+            if (itemForm.milkshake[i].checked === true){
+                newOrder.innerHTML += itemForm.milkshake[i].value;
+            }
+        }
+        cart.appendChild(newOrder);
+        let price = parseFloat(itemForm.price.innerHTML);
+        totalCart(price)
+        localStorage.setItem("cart",cart.innerHTML);
     }
-}
-newOrder.appendChild(br);
-newOrder.innerHTML += 'Milkshake';
-newOrder.appendChild(br);
 
-for (let i = 0; i < itemForm.milkshake.length;i++){
-    if (itemForm.milkshake[i].checked === true){
-        newOrder.innerHTML += itemForm.milkshake[i].value;
-    }
-}
-cart.appendChild(newOrder);
-let price = parseFloat(itemForm.price.innerHTML);
-totalCart(price)
 }
 function clearCart(){
     let cart = document.getElementById('cart');
     let total = document.getElementById('total');
+    let itemCount = document.getElementById('totalItemsInCart');
     total.innerHTML = '0';
     cart.innerHTML = '';
+    itemCount.innerHTML = '0';
+    localStorage.removeItem('cart');
+    localStorage.removeItem('cartTotal');
+
 
 }
 function totalCart(price){
+    let itemCounter = document.getElementById('totalItemsInCart');
+    let itemCounterInt = parseInt(itemCounter.innerHTML);
+    itemCounterInt++
     let total = document.getElementById('total');
     let totalNum = parseFloat(total.innerHTML);
     let newTotal = totalNum + price;
     total.innerHTML = newTotal.toString();
+    itemCounter.innerHTML = itemCounterInt.toString();
+    localStorage.setItem('cartTotal', total.innerHTML);
+}
+window.onload = function GetCart(){
+    let checkoutCart = document.getElementById('checkout-cart').innerHTML = localStorage.getItem('cart');
+    let checkoutTotal = document.getElementById('checkout-total').innerHTML = localStorage.getItem('cartTotal');
+
+}
+function showCustomerInfo(){
+    let cart = document.getElementById('checkout-cart-div').style.display = 'none';
+    let payment = document.getElementById('payment').style.display = 'none';
+    let customerInfo = document.getElementById('customer-information').style.display = 'block';
 }
